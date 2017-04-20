cf3f5f153d26e91f5880965352f9f93c713c117f
Jacky1100 <609615907@qq.com>
2017-04-20 15:58:56 +0800

    Jack update

--- src/main/java/com/paipianwang/pat/facade/information/service/PmsNewsFacade.java
+++ src/main/java/com/paipianwang/pat/facade/information/service/PmsNewsFacade.java
@@ -1,7 +1,10 @@
 package com.paipianwang.pat.facade.information.service;
 
 import java.util.List;
+import java.util.Map;
 
+import com.paipianwang.pat.common.entity.DataGrid;
+import com.paipianwang.pat.common.entity.PageParam;
 import com.paipianwang.pat.facade.information.entity.PmsNews;
 
 /**
@@ -12,5 +15,40 @@ import com.paipianwang.pat.facade.information.entity.PmsNews;
 public interface PmsNewsFacade {
 
 	public List<PmsNews> RecommendNews();
+	
+	/**
+	 * 根据ID查找新闻实体
+	 * @param newsId 新闻ID
+	 * @return
+	 */
+	public PmsNews findNewsById(long newsId);
+	
+	/**
+	 * 获取新闻列表
+	 * @param pageMap
+	 * @param pageParam
+	 * @return
+	 */
+	public DataGrid<PmsNews> listWithPagination(PageParam pageParam, Map<String, Object> paramMap);
+	
+	/**
+	 * 保存新闻实体
+	 * @param pmsNews
+	 * @return
+	 */
+	public long insert(PmsNews pmsNews);
 
+	/**
+	 * 更新新闻实体
+	 * @param pmsNews
+	 * @return
+	 */
+	public long update(PmsNews pmsNews);
+	
+	/**
+	 * 根据新闻ID，批量删除新闻实体
+	 * @param ids
+	 * @return
+	 */
+	public long deleteByIds(long[] ids);
 }
