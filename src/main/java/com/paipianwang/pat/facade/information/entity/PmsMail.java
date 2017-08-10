fecec0626d33124c2c5c0017b6e78ed6a06b6049
lirui0 <rui000x@foxmail.com>
2017-08-10 10:32:44 +0800

    mail

--- src/main/java/com/paipianwang/pat/facade/information/entity/PmsMail.java
+++ src/main/java/com/paipianwang/pat/facade/information/entity/PmsMail.java
@@ -21,6 +21,26 @@ public class PmsMail extends BaseEntity{
 	private String createTime = null;
 	private String updateTime = null;
 	
+	/**
+	 * 收件人
+	 */
+	private String receiver;
+	private String receiverRole;
+	/**
+	 * 抄送人
+	 */
+	private String bcc;
+	private String bccRole;
+	/**
+	 * 发件人
+	 */
+	private String sender;//邮箱-以逗号分隔
+	private String senderRole;
+	/**
+	 * 附件
+	 */
+	private String mailFile;
+	
 	public String getSubject() {
 		return subject;
 	}
@@ -51,4 +71,47 @@ public class PmsMail extends BaseEntity{
 	public void setUpdateTime(String updateTime) {
 		this.updateTime = updateTime;
 	}
+	public String getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+	public String getReceiverRole() {
+		return receiverRole;
+	}
+	public void setReceiverRole(String receiverRole) {
+		this.receiverRole = receiverRole;
+	}
+	public String getBcc() {
+		return bcc;
+	}
+	public void setBcc(String bcc) {
+		this.bcc = bcc;
+	}
+	public String getBccRole() {
+		return bccRole;
+	}
+	public void setBccRole(String bccRole) {
+		this.bccRole = bccRole;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getSenderRole() {
+		return senderRole;
+	}
+	public void setSenderRole(String senderRole) {
+		this.senderRole = senderRole;
+	}
+	public String getMailFile() {
+		return mailFile;
+	}
+	public void setMailFile(String mailFile) {
+		this.mailFile = mailFile;
+	}
+	
 }
